402ea804b6dd64fd57b84f4391f4108e0eb192bc
Clarissa Jaime <clarissanjaime@gmail.com>
2020-02-18 19:52:10 -0800

    Complete getNextUniqueId

--- datastore/counter.js
+++ datastore/counter.js
@@ -38,9 +38,12 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
-  return zeroPaddedNumber(counter);
+exports.getNextUniqueId = (callback) => {
+  readCounter((err, fileData) => {
+    counter = fileData;
+    counter = counter + 1;
+    writeCounter (counter, callback);
+  });
 };
 
 
